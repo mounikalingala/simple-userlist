05c4d5c547e8a93fa7c6375a628665ac18a829bd
mounika <mounikagoudlingala@gmail.com>
2023-04-13 14:03:50 +0530

    complete user input form with useRef

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,12 +1,16 @@
+import React from "react"
+import ReactDOM from "react-dom"
 import Button from "./Button"
 import Card from "./Card";
 import classes from "./ErrorModal.module.css";
 
-const ErroeModal = (props) => {
+const Backdrop = (props) => {
+    return <div className={classes.backdrop} onClick={props.onConfirm}/>
+}
+
+const ModalOverlay = props => {
     return (
-        <div>
-            <div className={classes.backdrop} onClick={props.onconfirm} />
-        <Card className={classes.modal} > 
+       <Card className={classes.modal} > 
             <header className={classes.header}>
                 <h2 className={classes.header}>{props.title}</h2>
         </header>
@@ -17,7 +21,15 @@ const ErroeModal = (props) => {
                     <Button onClick={props.onConfirm}>Okay</Button>
         </footer>
             </Card>
-        </div>
+   )
+}
+
+const ErroeModal = (props) => {
+    return (
+        <>
+            {ReactDOM.createPortal(<Backdrop onClick={props.onConfirm} />, document.getElementById('backdrop-root'))}
+            {ReactDOM.createPortal(<ModalOverlay title={props.title}  message={props.title}  onConfirm={props.onConfirm} />, document.getElementById('backdrop-root'))}
+        </>
     )
 }
 
--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,45 +1,40 @@
-import React, {useState} from "react"
+import React, {useState,useRef} from "react"
 import Card from "../UI/Card" 
-import Button from "../UI/Button" 
+import Button from "../UI/Button"
 import ErrorModal from "../UI/ErrorModal";
 import classes from "./AddUser.module.css"
 
 const AddUser = (props) => {
-    const [userName, setUserName] = useState("")
-    const [userAge, setUserAge] = useState("")
-    const [error,setError]=useState()
+    const nameInputRef = useRef()
+    const ageInputRef = useRef()
+    
+    const [error, setError] = useState()
 
     const addUserHandler = event => {
+        const enteredName = nameInputRef.current.value
+        const enteredAge=ageInputRef.current.value
         event.preventDefault()
 
-        if (userName.trim().length === 0 || userAge.trim().length === 0) {
+        if (enteredName.trim().length === 0 || enteredAge.trim().length === 0) {
             setError({
                 title: "Invalid input",
                 message: "Plase enter a valid name and age (non-emty values)"
             })
             return;
         }
-        if (+userAge < 1) {
+        if (+enteredAge < 1) {
             setError({
                 title: "Invalid age",
                 message: "Plase enter a valid age (> 0)"
             })
             return;
         }
-        console.log(userName,userAge)
-        props.onAddUser(userName, userAge)
-        setUserName('')
-        setUserAge('')
+        props.onAddUser(enteredName, enteredAge)  
+        nameInputRef.current.value = "";
+        ageInputRef.current.value = "";
     }
     
-    const onChageUserName = (event) => {
-        setUserName(event.target.value)
-    }
-
-    const onChageAge = (event) => {
-        setUserAge(event.target.value)
-    }
-
+    
     const errorHandler = () => {
         setError(null)
     }
@@ -50,13 +45,19 @@ const AddUser = (props) => {
         <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
             <label htmlfor="username">USERNAME</label>
-            <input id="username" type="text" value={userName} onChange={onChageUserName}/>
+            <input ref={nameInputRef} id="username" type="text"/>
             <label htmlfor="age" >AGE</label>
-            <input id="age" type="number" value={userAge}  onChange={onChageAge}/>
-            <Button type="submit">Add User</Button>
+            <input ref={ageInputRef} id="age" type="number" />
+            <Button  type="submit">Add User</Button>
             </form>
             </Card>
         </div>
     )
 }
 export default AddUser
+
+
+
+
+
+
